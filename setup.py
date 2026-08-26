f8e2782a816b3bd302b20dad4e7e174df76a45d2
Giovanni D'Agnese <jovannidagnese2@gmail.com>
2026-08-26 19:41:43 +0200

    packaging: add setup.py and pyproject.toml for PyPI release

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dagnese-fno",
+    version="0.1.0",
+    author="Giovanni D'Agnese",
+    author_email="jovannidagnese2@gmail.com",
+    description="Diffeomorphic Implicit Fourier Neural Operators with D'Agnese Topological Barrier Loss",
+    long_description=open("README.md").read(),
+    long_description_content_content_type="text/markdown",
+    url="https://github.com/GiovanniDAgnese-paper/DIF-FNO",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "torch>=2.0.0",
+        "numpy",
+    ],
+)
